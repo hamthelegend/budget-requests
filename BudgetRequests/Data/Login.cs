a002a67ce41dd16408e4e71f4cfcb7ea2dd91f8f
hamthelegend <hamuelxd@gmail.com>
2023-05-09 09:01:39 +0800

    Cleaned up code for logging in

---
+++ BudgetRequests/Data/Login.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+using BudgetRequests.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace BudgetRequests.Data;
+
+public static class Login
+{
+    public static readonly AuthenticationProperties AuthProperties = new AuthenticationProperties
+    {
+        AllowRefresh = true,
+        ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(1440),
+        IsPersistent = true,
+        IssuedUtc = DateTimeOffset.UtcNow
+    };
+
+    public static ClaimsIdentity GetClaimIdentity(User user)
+    {
+        var claims = new List<Claim>()
+        {
+            new("UserId", user.Id.ToString())
+        };
+
+        return new ClaimsIdentity(
+            claims, CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+}
