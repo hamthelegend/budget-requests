053468bb30f1ef1d19b0f1801c003ebac7144dd7
hamthelegend <hamuelxd@gmail.com>
2023-05-20 11:02:21 +0800

    Fixed errors with database creation

--- BudgetRequests/Models/BudgetRequests/AdminSignatory.cs
+++ BudgetRequests/Models/BudgetRequests/AdminSignatory.cs
@@ -1,12 +1,11 @@
-﻿using BudgetRequests.Models.BudgetRequests;
-using BudgetRequests.Models.Organizations;
+﻿using BudgetRequests.Models.Admins;
 
-namespace BudgetRequests.Models.Admins;
+namespace BudgetRequests.Models.BudgetRequests;
 
 public class AdminSignatory
 {
     public int Id { get; set; }
     public BudgetRequest BudgetRequest { get; set; }
-    public AdminRole Role { get; set; }
+    public AdminRole? Role { get; set; }
     public bool HasSigned { get; set; }
 }
--- BudgetRequests/Models/BudgetRequests/OfficerSignatory.cs
+++ BudgetRequests/Models/BudgetRequests/OfficerSignatory.cs
@@ -6,6 +6,6 @@ public class OfficerSignatory
 {
     public int Id { get; set; }
     public BudgetRequest BudgetRequest { get; set; }
-    public OfficerRole Role { get; set; }
+    public OfficerRole? Role { get; set; }
     public bool HasSigned { get; set; }
 }
